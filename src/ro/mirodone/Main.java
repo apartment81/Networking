f2743d47fcc93a6d98d50df8c6bc2edac7539824
apartment81 <bogdan.mirodone@gmail.com>
2019-05-09 01:42:27 +0300

    URL read page

--- src/ro/mirodone/Main.java
+++ src/ro/mirodone/Main.java
@@ -1,36 +1,52 @@
 package ro.mirodone;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 public class Main {
 
     public static void main(String[] args) {
 
+// using base URI with relatives URIs
+       // try {
+            // URI uri = new URI("db://username:password@myserver.com:5000/catalog/phones?os=android#samsung");
+            // URI uri = new URI("http://username:password@myserver.com:5000/catalog/phones?os=android#samsung");
 
-        try{
-           // URI uri = new URI("db://username:password@myserver.com:5000/catalog/phones?os=android#samsung");
-            URI uri = new URI("http://username:password@myserver.com:5000/catalog/phones?os=android#samsung");
+/*            URI baseUri = new URI("http://username:password@myserver.com:5000");
+            URI uri1 = new URI("/catalog/phones?os=android#samsung");
+            URI uri2 = new URI("/catalog/tvs?manufacturer=samsung");
+            URI uri3 = new URI("/stores/locations?zip=12345");
 
-            URL url = uri.toURL();
-            System.out.println("Url = " + url);
+            URI resolvedUri1 = baseUri.resolve(uri1);
+            URI resolvedUri2 = baseUri.resolve(uri2);
+            URI resolvedUri3 = baseUri.resolve(uri3);
 
-            System.out.println("Scheme = " + uri.getScheme());
-            System.out.println("Scheme specific-part = " + uri.getSchemeSpecificPart());
-            System.out.println("Authority = " + uri.getAuthority());
-            System.out.println("User info = " + uri.getUserInfo());
-            System.out.println("Host = " + uri.getHost());
-            System.out.println("Port = " + uri.getPort());
-            System.out.println("Path = " + uri.getPath());
-            System.out.println("Query = " + uri.getQuery());
-            System.out.println("Fragment = " + uri.getFragment());
+            URL url1 = resolvedUri1.toURL();
+            System.out.println("Url = " + url1);
+            URL url2 = resolvedUri2.toURL();
+            System.out.println("Url = " + url2);
+            URL url3 = resolvedUri3.toURL();
+            System.out.println("Url = " + url3);*/
 
-        }catch (URISyntaxException e){
-            System.out.println("URI Bad Syntax: " + e.getMessage());
-        }catch (MalformedURLException e){
-            System.out.println("URL Wrong: " + e.getMessage());
+            //read webpage with URL
+            try {
+                URL url = new URL("http://example.org");
+
+
+
+                BufferedReader inputStream = new BufferedReader(
+                        new InputStreamReader(url.openStream()));
+                String line = "";
+                while (line != null){
+                    line = inputStream.readLine();
+                    System.out.println(line);
+                }
+                inputStream.close();
+
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
     }
-}
